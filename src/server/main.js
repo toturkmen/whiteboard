0aacdff5aa00a4c5fb5ffef40a2d860021110857
Tolga <toturkmen@gmail.com>
2021-05-02 01:45:23 +0300

    Add: rooms

--- src/server/main.js
+++ src/server/main.js
@@ -5,11 +5,17 @@ const io = require('socket.io')(http);
 const port = process.env.PORT || 3000;
 const path = require('path');
 
-app.use(express.static(path.resolve(__dirname, '../client')));
+app.use(express.static(path.resolve(__dirname, './../client')));
+
+app.get('/:room', function (request, response) {
+  response.sendFile(path.resolve(__dirname, './../client/index.html'));
+});
 
 io.on('connection', (socket) => {
+  let room = socket.handshake.query.room;
+  socket.join(room);
   socket.on('timeline', (data) => {
-    socket.emit('timeline', data);
+    socket.to(room).emit('timeline', data);
   });
 });
 
